f81740ece70ec4ccff2a18d08acb457a3c755001
StefanLaux <stefan.laux@edu.tbz.ch>
2023-01-10 14:45:34 +0100

    k05

---
+++ K05/Programm/K05/K05/src/Bitcoin.java
@@ -0,0 +1,26 @@
+public class Bitcoin implements Coin{
+    private KryptoMiner[] miner;
+    private Kurs kurs;
+    private Netzwerk netzwerk;
+    private Börse börse;
+
+    private int blockNum;
+
+
+    public Bitcoin(KryptoMiner[] miner, Börse börse) {
+        this.miner = miner;
+        this.kurs = new Kurs( 200, 100);
+        this.netzwerk = new Netzwerk(this.miner, 10, 100);
+        this.börse = börse;
+    }
+
+    @Override
+    public int getBlockNumber() {
+        return this.blockNum;
+    }
+
+    @Override
+    public int setBlock() {
+        return this.blockNum++;
+    }
+}
